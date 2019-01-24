4fb48ad5f9d5293679eb7ce3dcf18f5669099a70
Eric Guzmán <ericmguzmanc@gmail.com>
2019-01-23 20:27:45 -0400

    Person by params and querystring

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ let express = require('express');
 let app = express();
 let personRoute = require('./routes/person');
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toString()} => ${req.originalUrl}`);
+  
+  next();
+});
 app.use(personRoute);
 app.use(express.static('public'));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('\x1b[33m%s\x1b[0m',`Server has started on port ${PORT}`));
--- src/routes/person.js
+++ src/routes/person.js
@@ -1,8 +1,20 @@
 let express = require('express');
 let router = express.Router();
 
+// QueryString => query property on request object
+// localhost:3000/person?name=thomas&age=20
 router.get('/person', (req, res) => {
-  res.send('You have requeste a person');
+  if (req.query.name) {
+    res.send(`You have requeste a person ${req.query.name}`);
+  } else {
+    res.send('You have requeste a person');
+  }
 });
+
+// Params property on the request object
+router.get('/person/:name', (req, res) => {
+  res.send(`You have requeste a person ${req.params.name}`);
+});
+
 
 module.exports = router;
